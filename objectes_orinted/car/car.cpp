45bbfe01b61734787e144fa99fb96e5957965379
mohammed-darrag <mohammeddarrag13@gmail.com>
2024-12-16 23:46:47 +0100

    CarManagementSystem_ConstructorDemo

---
+++ objectes_orinted/car/car.cpp
@@ -0,0 +1,31 @@
+#include "car.h"
+
+// Setter for maker
+void car::setmaker(string m) {
+    maker = m;
+}
+
+// Getter for maker
+string car::getmaker() {
+    return maker;
+}
+
+// Setter for model
+void car::setmodel(int m) {
+    model = m;
+}
+
+// Getter for model
+int car::getmodel() {
+    return model;
+}
+
+// Setter for color
+void car::setcolor(string c) {
+    color = c;
+}
+
+// Getter for color
+string car::getcolor() {
+    return color;
+}
